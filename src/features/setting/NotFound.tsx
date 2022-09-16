23e206331ea358f58c98eb40e5126c6f3429f01a
Lumend <yinminzhi1230@gmail.com>
2022-09-16 23:50:07 +0800

    release

--- src/features/setting/NotFound.tsx
+++ src/features/setting/NotFound.tsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom"
-const msg = '404 Not Found'
+const msg = "404 Not Found"
 
 function SettingIndex() {
-    return (<div>
-        <h1>{ msg }</h1>
-        <Link to="/">Back Home</Link>
-    </div>)
+  return (
+    <div>
+      <h1>izhi.io</h1>
+      <p>{msg}</p>
+      <Link to="https://www.lizhi.io">返回首页</Link>
+    </div>
+  )
 }
 
 export default SettingIndex
